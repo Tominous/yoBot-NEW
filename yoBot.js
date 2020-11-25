88964ba109d2dcbda118bef1447e9f7f0692e189
Yochran <71285258+Yochran@users.noreply.github.com>
2020-11-25 12:47:16 -0500

    Update yoBot.js

--- yoBot.js
+++ yoBot.js
@@ -112,28 +112,4 @@ bot.on("message", (msg) => {
     }
 })
 
-function convertTime(ms) {
-    const days = Math.floor(ms / (24 * 60 * 60 * 1000))
-    const daysms = ms % (24 * 60 * 60 * 1000)
-    const hours = Math.floor((daysms) / (60*60*1000))
-    const hoursms = ms % (60 * 60 * 1000)
-    const minutes = Math.floor((hoursms) / (60 * 1000))
-    const minutesms = ms % (60 * 1000)
-    const sec = Math.floor((minutesms) / (1000))
-    let time = ""
-    if (days > 0) {
-        time = time + days + "d "
-    }
-    if (hours > 0) {
-        time = time + hours + "h "
-    }
-    if (minutes > 0) {
-        time = time + minutes + "m "
-    }
-    if (sec > 0) {
-        time = time + sec + "s"
-    }
-    return time
-}
-
 bot.login(config.Token);
